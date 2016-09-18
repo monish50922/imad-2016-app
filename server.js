73b994a29c3c17b206ed872c1d5b4d64e4222a19
monish50922 <poojarimonish53@gmail.com>
2016-09-18 11:45:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/Article-one',function(req,res){
+    res.send('Article one is resquested and will be serve here ...!');
+});
+app.get('/Article-two',function(req,res){
+    res.send('Article two is requested and will be serve here ...!');
+});
+app.get('/Article-three',function(req,res){
+    res.send('Article three is requested and will be serve here ...!');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
