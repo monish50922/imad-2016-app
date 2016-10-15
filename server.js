70a59c250138ff0edfe77e740b2e4735d675d4d8
monish50922 <poojarimonish53@gmail.com>
2016-10-15 15:41:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,8 +121,8 @@ app.get('/ui/main1.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 
