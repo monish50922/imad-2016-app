68b303cc8280ee874976013e1deedb98d045ca18
monish50922 <poojarimonish53@gmail.com>
2016-10-26 23:19:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,6 +106,13 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/:Articlename',function(req,res){
     //Articlename=Article-one
     var Articlename = req.params.Articlename;
@@ -125,11 +132,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var counter=0;
-app.get('/counter',function(req,res){
-    counter=counter+1;
-    res.send(counter.toString());
-});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
