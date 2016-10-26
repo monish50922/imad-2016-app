50d068501ee2a4297e73593c7ae5d402a9dd71a3
monish50922 <poojarimonish53@gmail.com>
2016-10-26 23:36:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,37 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 
 
-var img = document.getElementById('madi')
-var marginLeft= 0 ;
+//var img = document.getElementById('madi')
+//var marginLeft= 0 ;
 
-function moveRight(){
-    marginLeft=marginLeft+1;
+//function moveRight(){
+ //   marginLeft=marginLeft+1;
     
-    img.style.marginLeft=marginLeft + 'px';
+   // img.style.marginLeft=marginLeft + 'px';
     
     
-}
+//}
 
-img.onclick=function(){
-    //img.style.marginLeft='100px';
-    var interval=setInterval(moveRight,50);
+//img.onclick=function(){
+ //   //img.style.marginLeft='100px';
+ //   var interval=setInterval(moveRight,50);
     
-}
+//}
+
+//counter Code
+var button=document.getElementById('counter');
+var counter = 0;
+
+button.onclick=function (){
+    //make a request for counter end point
+    
+    //capture the response and store it in a variabe
+    
+    //render the variable in a correct span
+    counter =counter +1 ;
+    var span=document.getElementById('counter');
+    span.innerHTML=counter.toString();
+    
+    
+};
+
