cadb8c1aa3e4d5bc2c402f4c96581447d19e4004
monish50922 <poojarimonish53@gmail.com>
2016-10-28 11:05:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,45 +5,6 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
-    
-var htmltemplate =`
-    <html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <link href="/ui/style.css" rel="stylesheet" />
-     
-    </head>
-
-    <body>
-        <div class="container">
-        
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-           ${heading}
-        </h3>
-        <div>
-            Sept18,2016
-        </div>
-        <div> 
-            ${content}
-        </div>
-    </div>
-    </body>
-</html>
-`;
-return htmltemplate;
-
-}
 
 
 var articles={
@@ -104,6 +65,45 @@ var articles={
     }
     
 };
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+var htmltemplate =`
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+     
+    </head>
+
+    <body>
+        <div class="container">
+        
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+           ${heading}
+        </h3>
+        <div>
+            Sept18,2016
+        </div>
+        <div> 
+            ${content}
+        </div>
+    </div>
+    </body>
+</html>
+`;
+return htmltemplate;
+
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
