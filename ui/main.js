baebff2443c6eb7ae91191eb27a66bfef55d9c76
monish50922 <poojarimonish53@gmail.com>
2016-11-01 09:25:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -56,3 +56,23 @@ button.onclick = function (){
     
 };
 
+//submit name 
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementByID('submitbtn');
+
+submit.onclick=function(){
+    //MAke the request to server and send name 
+    //capture the list of names and render it as a list 
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for (var i=0;i<names.length;i++){
+        list+='<li>'+name[i]+'/li';
+        
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+};
+
+
